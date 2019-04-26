b8bcb4cf26a60bfcfd7a17ebc8e1dfc2d262566a
nadine-shi <nadine@afterlogic.com>
2019-04-26 15:16:23 +0300

    License and other texts improving

--- Module.php
+++ Module.php
@@ -8,6 +8,8 @@
 namespace Aurora\Modules\StandardAuth;
 
 /**
+ * This module provides API for authentication by login/password that relies on database.
+ * 
  * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
  * @license https://afterlogic.com/products/common-licensing Afterlogic Software License
  * @copyright Copyright (c) 2019, Afterlogic Corp.
